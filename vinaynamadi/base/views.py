fe0d0215bb5afe70440c95da9ef8ae12d5226b57
VinayN03 <vinaykumar.namadi@gmail.com>
2020-11-23 00:23:36 +0530

    created basic templates

--- vinaynamadi/base/views.py
+++ vinaynamadi/base/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h2>Home<h2>')
+    return render(request, 'base/home.html')
+
 
 def posts(request):
-    return HttpResponse('<h2>Posts<h2>')
+    return render(request, 'base/posts.html')
+
 
 def post(request):
-    return HttpResponse('<h2>Post Title<h2>')
+    return render(request, 'base/post.html')
+
 
 def profile(request):
-    return HttpResponse('<h2>User Profile<h2>')
-
+    return render(request, 'base/profile.html')
